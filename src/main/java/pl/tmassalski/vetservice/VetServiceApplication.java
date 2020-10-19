d0f081c00a37873a279fb8cf0e2d36d80fed1b36
Tomasz Massalski <tomasz.massalski@gmail.com>
2020-10-19 11:03:37 +0200

    Initial commit

---
+++ src/main/java/pl/tmassalski/vetservice/VetServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.tmassalski.vetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/tmassalski/vetservice/VetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.tmassalski.vetservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
