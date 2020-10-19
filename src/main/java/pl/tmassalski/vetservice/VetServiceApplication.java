c2ef88f045e86d758ef49add35ca58bf3cfb4252
Tomasz Massalski <tomasz.massalski@gmail.com>
2020-10-19 12:39:34 +0200

    Vet, Specialty, Enable Auditing

--- src/main/java/pl/tmassalski/vetservice/VetServiceApplication.java
+++ src/main/java/pl/tmassalski/vetservice/VetServiceApplication.java
@@ -2,8 +2,12 @@ package pl.tmassalski.vetservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableJpaRepositories
 public class VetServiceApplication {
 
 	public static void main(String[] args) {
