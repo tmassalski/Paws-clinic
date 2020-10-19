1037bb529debf8bfcf6bb10ad118b9bbf9ee4a11
Tomasz Massalski <tomasz.massalski@gmail.com>
2020-10-19 14:13:38 +0200

    Enable Swagger and bean validation

---
+++ src/main/java/pl/tmassalski/vetservice/api/configuration/SwaggerConfiguration.java
@@ -0,0 +1,21 @@
+package pl.tmassalski.vetservice.api.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfiguration {
+
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
