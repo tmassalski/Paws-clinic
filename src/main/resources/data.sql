6cd74599d275a8211bf479f93d7d92a6012ae8ef
Tomasz Massalski <tomasz.massalski@gmail.com>
2020-11-10 22:28:03 +0100

    fix dates in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -17,22 +17,22 @@ INSERT INTO owner VALUES
 (1, 'Bag End', 'Shire', 'Bilbo', 'Baggins', '777666777'),
 (2, 'Moria', 'Khazad-dum', 'Gandalf', 'Grey', '111333444'),
 (3, 'Nostromo', 'Space', 'Ellen', 'Ripley', '445544666'),
-(4, 'Griffindor', 'Hogward', 'Harry', 'Potter', '000999000');
+(4, 'Gryffindor', 'Hogwart', 'Harry', 'Potter', '000999000');
 
 INSERT INTO pet VALUES
-(1, '1.1.1937', 'Smaug', 1, 3),
-(2, '6.6.666', 'Durin''s Bane', 2, 12),
-(3, '1.6.1973', 'Queen', 3, 13),
-(4, '12.12.1212', 'Fawkes', 4, 11);
+(1, '1937-1-1', 'Smaug', 1, 3),
+(2, '666-6-6', 'Durin''s Bane', 2, 12),
+(3, '1973-6-1', 'Queen', 3, 13),
+(4, '2012-12-12', 'Fawkes', 4, 11);
 
 INSERT INTO visit VALUES
-(1, '1.2.2020', 'Fixed loose scale', 1),
-(2, '6.3.2020', 'Trimmed horns', 2),
-(3, '17.4.2020', 'Dental hygiene', 3),
-(7, '17.5.2020', 'Dental hygiene', 3),
-(4, '20.5.2020', 'Replaced burnt feather', 4),
-(5, '20.6.2020', 'Replaced burnt feather', 4),
-(6, '20.7.2020', 'Replaced burnt feather', 4);
+(1, '2020-2-1', 'Fixed loose scale', 1),
+(2, '2020-3-6', 'Trimmed horns', 2),
+(3, '2020-4-17', 'Dental hygiene', 3),
+(7, '2020-5-17', 'Dental hygiene', 3),
+(4, '2020-5-20', 'Replaced burnt feather', 4),
+(5, '2020-6-20', 'Replaced burnt feather', 4),
+(6, '2020-7-20', 'Replaced burnt feather', 4);
 
 INSERT INTO specialty VALUES
 (1, 'surgery'),
@@ -49,5 +49,3 @@ INSERT INTO vet_specialties VALUES
 (1,1),
 (1,2),
 (1,3);
-
-
